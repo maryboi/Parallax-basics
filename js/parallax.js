64cce5878d9ce2f5516752cd1ed4605f45f0ae0c
Mary Aberg <maryaberg@me.com>
2013-10-20 14:09:45 +0800

    first commit

---
+++ js/parallax.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$(window).bind('scroll', function(e){
+		parallax();
+
+});
+
+});
+
+
+function parallax(){
+	var scrollPosition = $(window).scrollTop();
+	$('#grid').css('top', (0 - (scrollPosition *.2))+'px' );
+}
